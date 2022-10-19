cbd7d815eb7255bf170f4d78e25b0d2950308b8a
marcoDmc <marcodamasceno07@gmail.com>
2022-10-19 15:03:00 -0300

    :sparkles: added function that controls the favorites tab and all projects

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -2,16 +2,22 @@ import module from "./Sidebar.module.sass";
 import { TiThLargeOutline } from "react-icons/ti";
 import { TbMessageShare } from "react-icons/tb";
 import { IoSquareOutline } from "react-icons/io5";
-import { IoIosNotificationsOutline, IoIosArrowDown } from "react-icons/io";
+import {
+  IoIosNotificationsOutline,
+  IoIosArrowDown,
+  IoIosArrowUp,
+} from "react-icons/io";
 import { SlCheck } from "react-icons/sl";
 import { FiUsers } from "react-icons/fi";
 import { BsGear } from "react-icons/bs";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [controlFavorite, setControlFavorite] = useState(false);
+  const [controlAll, setControlAll] = useState(false);
   return (
     <aside className={module.aside}>
       <div className={module.wrapper_icons}>
-        {/* <div className={module.letter_c}>c</div> */}
         <span className={module.span}>
           <TiThLargeOutline className={module.window} />
         </span>
@@ -35,12 +41,26 @@ const Sidebar = () => {
         </span>
       </div>
       <div className={module.wrapper_tables}>
-        <ul className={module.favorite}>
-          <IoIosArrowDown className={module.arrow_down} />
+        <ul
+          className={module.favorite}
+          onClick={() => setControlFavorite((prev) => !prev)}
+        >
+          {controlFavorite ? (
+            <IoIosArrowDown className={module.arrow_down} />
+          ) : (
+            <IoIosArrowUp className={module.arrow_up} />
+          )}
           favorites
         </ul>
-        <ul className={module.all_projects}>
-          <IoIosArrowDown className={module.arrow_down} />
+        <ul
+          className={module.all_projects}
+          onClick={() => setControlAll((prev) => !prev)}
+        >
+          {controlAll ? (
+            <IoIosArrowDown className={module.arrow_down} />
+          ) : (
+            <IoIosArrowUp className={module.arrow_up} />
+          )}
           all projects
         </ul>
       </div>
