55cc4392dc5b8246e7ff6f1dd9fd2db2f9eae5e2
marcoDmc <marcodamasceno07@gmail.com>
2022-10-21 12:33:01 -0300

    :recycle: added new task type component parameters

--- components/TypeTask/TypeTask.jsx
+++ components/TypeTask/TypeTask.jsx
@@ -1,8 +1,12 @@
 import module from "./TypeTask.module.sass";
 
-const TypeTask = ({ type, color }) => {
+const TypeTask = ({ type, color, textColor, typetitle }) => {
   return (
-    <span className={module.typetask} style={{ backgroundColor: color }}>
+    <span
+      className={module.typetask}
+      style={{ backgroundColor: color, color: textColor }}
+      title={typetitle}
+    >
       {type}
     </span>
   );
