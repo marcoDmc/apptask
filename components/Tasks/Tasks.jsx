9e425a0f684bddc6a84c893e7a5dd7c7f0e28906
marcoDmc <marcodamasceno07@gmail.com>
2022-10-21 18:30:28 -0300

    :sparkles: created option to edit or delete task

--- components/Tasks/Tasks.jsx
+++ components/Tasks/Tasks.jsx
@@ -2,6 +2,8 @@
 import TypeTask from "../TypeTask/TypeTask";
 import module from "./Tasks.module.sass";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { AiTwotoneDelete, AiTwotoneEdit } from "react-icons/ai";
+import { useState } from "react";
 
 const Tasks = ({
   typeOne,
@@ -12,8 +14,10 @@ const Tasks = ({
   title,
   content,
 }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={module.task}>
+    <div className={module.task} onMouseLeave={() => setOpen(false)}>
       <span className={module.wrapper_types}>
         <span className={module.wrapper}>
           <TypeTask
@@ -30,7 +34,25 @@ const Tasks = ({
             textColor="#fff"
           />
         </span>
-        <BsThreeDotsVertical className={module.dot} />
+        <BsThreeDotsVertical
+          className={module.dot}
+          onClick={() => setOpen((prev) => !prev)}
+        />
+        <div
+          className={module.popup}
+          style={
+            open && title == title ? { display: "flex" } : { display: "none" }
+          }
+        >
+          <span className={module.opt}>
+            <AiTwotoneEdit className={module.edit} />
+            <p>edit</p>
+          </span>
+          <span className={module.opt}>
+            <AiTwotoneDelete className={module.delete} />
+            <p>delete</p>
+          </span>
+        </div>
       </span>
       <strong className={module.Title}>{title}</strong>
       <p className={module.content}>{content}</p>
