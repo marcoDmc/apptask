28944be63007ab7cc63d61a30c192abf751a232d
marcoDmc <marcodamasceno07@gmail.com>
2022-10-21 12:36:02 -0300

    :sparkles: new task creation component

---
+++ components/createTask/NewTask.jsx
@@ -0,0 +1,52 @@
+import module from "./NewTask.module.sass";
+import { IoCloseSharp } from "react-icons/io5";
+
+const NewTask = ({ newTask, handleCreateNewTask }) => {
+  const HandlePreventEvent = (e) => e.preventDefault();
+
+  return (
+    <div
+      className={module.wrapper}
+      style={newTask ? { display: "flex" } : { display: "none" }}
+    >
+      <form
+        action="post"
+        method="post"
+        className={module.form}
+        onClick={HandlePreventEvent}
+      >
+        <span className={module.label}>
+          create a new task{" "}
+          <IoCloseSharp
+            className={module.sharp}
+            onClick={handleCreateNewTask}
+          />
+        </span>
+
+        <span className={module.wrapper_title}>
+          {/* <label htmlFor={module.title}>title</label> */}
+          <input type="text" placeholder="title" className={module.title} />
+        </span>
+        <span className={module.wrapper_title}>
+          {/* <label htmlFor={module.task_type}>task type</label> */}
+          <input
+            type="text"
+            placeholder="type task"
+            className={module.task_type}
+          />
+        </span>
+        <span className={module.wrapper_title}>
+          {/* <label htmlFor={module.content}>content</label> */}
+          <textarea
+            className={module.content}
+            placeholder="content"
+            maxLength="200"
+          />
+        </span>
+        <button className={module.create_task}>create task</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTask;
