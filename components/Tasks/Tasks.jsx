4f9a692d533d2a5248c80bc23f3ec034b2b384eb
marcoDmc <marcodamasceno07@gmail.com>
2022-10-21 12:33:36 -0300

    :recycle: added new task type component parameters

--- components/Tasks/Tasks.jsx
+++ components/Tasks/Tasks.jsx
@@ -1,21 +1,39 @@
+/* eslint-disable react/no-unescaped-entities */
 import TypeTask from "../TypeTask/TypeTask";
 import module from "./Tasks.module.sass";
 import { BsThreeDotsVertical } from "react-icons/bs";
 
-const Tasks = () => {
+const Tasks = ({
+  typeOne,
+  typeTwo,
+  colorOne,
+  colorTwo,
+  typetitle,
+  title,
+  content,
+}) => {
   return (
     <div className={module.task}>
       <span className={module.wrapper_types}>
         <span className={module.wrapper}>
-          <TypeTask type="App" color="greenyellow" />
-          <TypeTask type="Website" color="skyblue" />
+          <TypeTask
+            type={typeOne}
+            color={colorOne}
+            titleOne={typetitle}
+            textColor="#000"
+          />
+          <TypeTask
+            type={typeTwo}
+            color={colorTwo}
+            titleOne={typetitle}
+            typetitle={typetitle}
+            textColor="#fff"
+          />
         </span>
         <BsThreeDotsVertical className={module.dot} />
       </span>
-      <strong className={module.Title}>pages "about" and "carrers"</strong>
-      <p className={module.content}>
-        all the detais are in the file. i'm sure it will turn out cool!
-      </p>
+      <strong className={module.Title}>{title}</strong>
+      <p className={module.content}>{content}</p>
     </div>
   );
 };
